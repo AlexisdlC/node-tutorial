c6d4612da6b1a65061c40ced17f708218a0246b6
AlexisdlC <alexis.delacotte@gmail.com>
2022-04-25 13:48:51 +0200

    Update to course

---
+++ 1-event-loop-exemples/2-setTimeout.js
@@ -0,0 +1,7 @@
+// sarted operating system process
+console.log('first task')
+setTimeout(()=>{
+    console.log('second')
+},0)
+console.log('third')
+// completed and exited operating system process
